d6bb5971911acd6ea751e74e652d10c14eeceef6
Pratthima <pratthima2901@gmail.com>
2026-09-05 10:23:48 +0530

    Time: 1 ms (99.89%) | Memory: 55.8 MB (31.74%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0,can=0;
+        for(int num: nums){
+            if(count==0){
+            can=num;
+            }
+            if(num==can){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+        }
+        return can;
+        
+    }
+}

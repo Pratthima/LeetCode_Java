e366a1b81b015bce4808f5c88d0f31b6a029405e
Pratthima <pratthima2901@gmail.com>
2026-09-12 12:03:15 +0530

    Time: 1 ms (83.66%) | Memory: 42.9 MB (18.42%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] word=s.split(" ");
+        if(pattern.length()!=word.length)
+        return false;
+        HashMap<Character,String>map=new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            char c=pattern.charAt(i);
+            String w=word[i];
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(w))
+                return false;
+            }
+            else{
+                if(map.containsValue(w))
+                return false;
+            }
+            map.put(c,w);
+            
+        }
+        return true;
+        
+    }
+}
